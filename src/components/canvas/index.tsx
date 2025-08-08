97bdecd4d5da6bca019e33a4dc925e675614f18e
李智 <729360726@qq.com>
2025-08-08 09:50:56 +0800

    style: 透明背景

--- src/components/canvas/index.tsx
+++ src/components/canvas/index.tsx
@@ -1,19 +1,35 @@
-import { CanvasRenderer } from "@/core/engine"
+import { CanvasRenderer } from "@/core/engine";
 import { SceneTree } from "@/core/models";
-import { useHotKey } from "@/hooks/useHotKey"
+import { useHotKey } from "@/hooks/useHotKey";
 import { getProjectState, setProjectState } from "@/store/project";
-import { useEffect } from "react"
+import { useEffect } from "react";
+import "./index.css";
+import { usePageState } from "@/store/page";
 
 export const MainCanvas = () => {
-    useHotKey();
-    useEffect(() => {
-        const elements = getProjectState('mockElements');
-        const sceneTree = new SceneTree();
-        sceneTree.build(elements);
-        CanvasRenderer.init(sceneTree).then(props => {
-            setProjectState({ sceneTree, CK: props?.canvasKit, surface: props?.surface })
-        });
-    }, [])
+  const pageFillPaint = usePageState("fillPaint");
+  useHotKey();
+  useEffect(() => {
+    const elements = getProjectState("mockElements");
+    const sceneTree = new SceneTree();
+    sceneTree.build(elements);
+    CanvasRenderer.init(sceneTree).then((props) => {
+      setProjectState({
+        sceneTree,
+        CK: props?.canvasKit,
+        surface: props?.surface,
+      });
+    });
+  }, []);
 
-    return <canvas id={'main-canvas'} />
-}
+  return (
+    <canvas
+      id={"main-canvas"}
+      className={
+        pageFillPaint.color[3] !== 1 || !pageFillPaint.visible
+          ? "checkerboard"
+          : "default-canvas"
+      }
+    />
+  );
+};
